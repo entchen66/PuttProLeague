bb9e368215de4f1d16569ecf9af2e13315a9c675
Chris <tiegerchris1@web.de>
2024-05-15 15:30:22 +0200

    initial commit

---
+++ src/main/java/de/foxy/proPuttLeauge/ProPuttLeaugeApplication.java
@@ -0,0 +1,13 @@
+package de.foxy.proPuttLeauge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProPuttLeaugeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProPuttLeaugeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/foxy/proPuttLeauge/ProPuttLeaugeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.foxy.proPuttLeauge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProPuttLeaugeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
